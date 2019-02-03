002972361e96c106771464f21fcacf6fb619d63b
thesparkilingsun <adityatiwari9931@gmail.com>
2019-02-02 21:23:16 -0800

    Added the search to UI, now need to match the string with the voice commands given and take action acc

--- app/src/main/java/com/example/memecreator/MainActivity.java
+++ app/src/main/java/com/example/memecreator/MainActivity.java
@@ -2,25 +2,36 @@ package com.example.memecreator;
 
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
 import android.media.Image;
 import android.media.MediaPlayer;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MotionEvent;
 import android.view.View;
 
 import android.view.Window;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 import static android.view.KeyEvent.ACTION_DOWN;
 
 public class MainActivity extends Activity   {
-
+    private static final int REQ_CODE_SPEECH_INPUT = 100;
     MediaPlayer mp ;
     private Integer images[] = {R.drawable.pic1};
     private int currImage = 0;
@@ -39,20 +50,28 @@ public class MainActivity extends Activity   {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageView img_meme;
-
-        final TextView text_display;
-        LinearLayout linearLayout;
+        SearchView search;
+        final TextView text_display,search_result;
+        RelativeLayout linearLayout;
         // Initializer
         img_meme = findViewById(R.id.meme_img);
-        linearLayout = (LinearLayout) findViewById(R.id.base_layout);
+
         text_display = findViewById(R.id.display_text);
+        search_result = findViewById(R.id.searchResult);
 
-        //setInitialImage();
-        switchImage();
+        // Create an array and then compare results
+        ImageButton searchButton;
+        searchButton = (ImageButton) findViewById(R.id.btnSpeak);
+        searchButton.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                startVoiceInput();
+            }
+        });
 
         // experimenting touch
-        linearLayout.setOnTouchListener(new View.OnTouchListener() {
+        img_meme.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
 
@@ -93,11 +112,34 @@ public class MainActivity extends Activity   {
                 return false;
             }
         });
+
+
+
     }
+
+
+    private void startVoiceInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
+        }
+    }
+
+
+
+
+
+
 
      private void switchImage(){
 
      }
+
 
 
     @Override
