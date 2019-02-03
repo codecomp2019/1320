4ba9e330a15db5d5a4fdd3e2c74433f4205a8e6c
thesparkilingsun <adityatiwari9931@gmail.com>
2019-02-03 10:50:06 -0800

    added transition, changed logo

--- app/src/main/java/com/example/memecreator/MainActivity.java
+++ app/src/main/java/com/example/memecreator/MainActivity.java
@@ -43,9 +43,11 @@ public class MainActivity extends Activity {
     private static final int REQ_CODE_SPEECH_INPUT = 100;
     //for Playing the intro audio
     MediaPlayer mp;
+
     //counter for key press
      int timer = 0;
-    private Integer images[] = {R.drawable.pic1}; // was creating a n array of images with int as pointer
+    int counter = 0;
+
     private TextView  search_result;
     TextView text_display;
     String message;
@@ -56,7 +58,7 @@ public class MainActivity extends Activity {
         setTheme(R.style.AppTheme); // Changing theme back to default app theme
         // Adding audio cue, to let know app started
         //Welcome Audio and Double Tap options
-        mp = MediaPlayer.create(getBaseContext(), R.raw.welcome_message);
+        mp = MediaPlayer.create(getBaseContext(), R.raw.welcomeaudio);
 
         // Start Audio
         mp.start();
@@ -133,6 +135,7 @@ public class MainActivity extends Activity {
             @Override
             public void onInit(int status) {
                 if (status == TextToSpeech.SUCCESS) {
+                    mp = MediaPlayer.create(getBaseContext(), R.raw.reading);
                     int ttsLang = textToSpeech.setLanguage(Locale.US);
 
                     if (ttsLang == TextToSpeech.LANG_MISSING_DATA
@@ -157,6 +160,7 @@ public class MainActivity extends Activity {
     // creating a method for data to be spoken by text anywhere they click
     public void textSpeak(){
 
+         // Reading Text transition
         String data = text_display.getText().toString();
         Log.i("TTS", "button clicked: " + data);
         int speechStatus = textToSpeech.speak(data, TextToSpeech.QUEUE_FLUSH, null);
@@ -205,23 +209,34 @@ public class MainActivity extends Activity {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                        search_result.setText(result.get(0));
                        String msg = result.get(0);
+                       // set the anaylze image
+
+                       // call the increment counter here
+                       incrementCounter();
                        if(msg.contains("next")){
-                             // Change to Next Image
+                             // Change to Next Images
+
+                           mp = MediaPlayer.create(getBaseContext(), R.raw.next);
+
+
                        }
                          else if(msg.contains("previous"))
                         { // Change to previous image}
-
+                            mp = MediaPlayer.create(getBaseContext(), R.raw.previous);
                         }
                         else if(msg.contains("quit"))
                             {
                                 // Quit Application
+                                mp = MediaPlayer.create(getBaseContext(), R.raw.terminateapp);
                             }
                        else if(msg.contains("analyze | image"))
                        {
                            // Analyze Image
+                           mp = MediaPlayer.create(getBaseContext(), R.raw.analyzingimage);
                        }
                        else if(msg.contains("read image"))
                        {
+                           mp = MediaPlayer.create(getBaseContext(), R.raw.reading);
                            // Read Image
                        }
 
@@ -232,6 +247,17 @@ public class MainActivity extends Activity {
         }
 
     }
+
+
+    // to read Image
+    public void incrementCounter(){
+        counter++;
+        if(counter >5){
+            counter = 0;
+        }
+
+    }
+
 
     // catches the onKeyDown button event
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -277,3 +303,4 @@ public class MainActivity extends Activity {
     }
 
 }
+
