0e4f17d880d4ab6392f61e1dd589803fbb185230
MikaelKuyumchyan <KuyumchyanM@gmail.com>
2019-02-02 19:41:45 -0800

    Second commit: SpeechToText

--- app/src/main/java/com/example/memecreator/TextToSpeach.java
+++ app/src/main/java/com/example/memecreator/TextToSpeach.java
@@ -1,78 +1,63 @@
 package com.example.memecreator;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
+import android.widget.ImageButton;
+import android.widget.TextView;
+import java.util.ArrayList;
 import java.util.Locale;
 
-public class TextToSpeach {
+public class TextToSpeach extends AppCompatActivity {
 
-    public class MainActivity extends AppCompatActivity {
-        private TextToSpeech textToSpeech;
-        private Button btn;
-        private EditText editText;
+    private static final int REQ_CODE_SPEECH_INPUT = 100;
+    private TextView mVoiceInputTv;
+    private ImageButton mSpeakBtn;
 
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            btn = (Button) findViewById(R.id.btn);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            editText = (EditText) findViewById(R.id.et);
-            // Start
-            textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-                @Override
-                public void onInit(int status) {
-                    if (status == TextToSpeech.SUCCESS) {
-                        int ttsLang = textToSpeech.setLanguage(Locale.US);
+        mVoiceInputTv = (TextView) findViewById(R.id.voiceInput);
+        mSpeakBtn = (ImageButton) findViewById(R.id.btnSpeak);
+        mSpeakBtn.setOnClickListener(new View.OnClickListener() {
 
-                        if (ttsLang == TextToSpeech.LANG_MISSING_DATA
-                                || ttsLang == TextToSpeech.LANG_NOT_SUPPORTED) {
-                            Log.e("TTS", "The Language is not supported!");
-                        } else {
-                            Log.i("TTS", "Language Supported.");
-                        }
-                        Log.i("TTS", "Initialization success.");
-                    } else {
-                        Toast.makeText(getApplicationContext(), "TTS Initialization failed!", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-            // End
-
-            btn.setOnClickListener(new View.OnClickListener() {
-
-                // Start
-                @Override
-                public void onClick(View arg0) {
-
-                    String data = editText.getText().toString();
-                    Log.i("TTS", "button clicked: " + data);
-                    int speechStatus = textToSpeech.speak(data, TextToSpeech.QUEUE_FLUSH, null);
-
-                    if (speechStatus == TextToSpeech.ERROR) {
-                        Log.e("TTS", "Error in converting Text to Speech!");
-                    }
-                }
-                // End
-
-            });
-        }
-
-        @Override
-        public void onDestroy() {
-            super.onDestroy();
-            if (textToSpeech != null) {
-                textToSpeech.stop();
-                textToSpeech.shutdown();
+            @Override
+            public void onClick(View v) {
+                startVoiceInput();
             }
+        });
+    }
+
+    private void startVoiceInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    mVoiceInputTv.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
 }
